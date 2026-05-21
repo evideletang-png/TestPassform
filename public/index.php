3ff4f3537d56d87e8ec10884d58a4404ff99bff0
evideletang-png <evi.deletang@gmail.com>
2026-05-21 13:43:48 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,19 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$requestPath = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
+
+if ($requestPath === '/_ping') {
+    header('Content-Type: application/json');
+    echo json_encode([
+        'ok' => true,
+        'php' => PHP_VERSION,
+        'time' => date(DATE_ATOM),
+        'entrypoint' => 'public/index.php',
+    ]);
+    exit;
+}
+
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
